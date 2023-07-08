0af12c80b608b706d1756ca09a016244e49be529
Samuel Borges <sammy_rb@hotmail.com>
2023-07-08 15:43:27 -0300

    first commit

---
+++ src/main/java/com/devsuperior/aulajparepository/JpaRepositoryBoladonaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.aulajparepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaRepositoryBoladonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaRepositoryBoladonaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/aulajparepository/JpaRepositoryBoladonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.aulajparepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaRepositoryBoladonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
